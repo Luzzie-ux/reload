d2201bd6754ef199fb7249e661322b2aa4e5f5ae
Luzzie-ux <estelarodrigoluz@gmail.com>
2026-04-13 21:10:21 +0100

    strlen

---
+++ ex16/ft_strlen.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 21:03:00 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/04/13 21:10:01 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*#include <stdio.h>
+int	main()
+{
+	char str[] = "this is a long sentence";
+	printf("%d\n", ft_strlen(str));
+}*/
