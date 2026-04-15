fe4bdff8bc48c9fda140a39370cebcdee002a31a
Rodrigo Pereira <rodrpere@c1r14s6.42porto.com>
2026-04-15 11:45:43 +0100

    ex25 adicionado

---
+++ ex25/ft_foreach.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 10:11:51 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/04/15 11:45:20 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+/*void	ft_putchar(char c);
+
+void	ft_putnbr(int nb);*/
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	int	i;
+
+	i = 0;
+	while (tab[i] < length)
+	{
+		f(tab[i]);
+		if (tab[i] == length - 1)
+		{
+			break ;
+		}
+		i++;
+	}
+}
+/*#include <stdio.h>
+int	main(void)
+{
+	int	tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int	len = sizeof(tab)/ sizeof(tab[0]);
+	ft_foreach(tab, len, &ft_putnbr);
+	return 0;
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}*/
